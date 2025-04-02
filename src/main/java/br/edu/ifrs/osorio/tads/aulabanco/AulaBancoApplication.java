ca3209dbb5237fc2b79f4f30a7a375ee2c83af16
0Chaves <danieldossantoschaves@gmail.com>
2025-04-02 16:07:48 -0300

    Remove .env

--- src/main/java/br/edu/ifrs/osorio/tads/aulabanco/AulaBancoApplication.java
+++ src/main/java/br/edu/ifrs/osorio/tads/aulabanco/AulaBancoApplication.java
@@ -1,5 +1,6 @@
 package br.edu.ifrs.osorio.tads.aulabanco;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AulaBancoApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
         SpringApplication.run(AulaBancoApplication.class, args);
     }
 
--- src/main/java/br/edu/ifrs/osorio/tads/aulabanco/repository/EnderecoRepository.java
+++ src/main/java/br/edu/ifrs/osorio/tads/aulabanco/repository/EnderecoRepository.java
@@ -2,6 +2,8 @@ package br.edu.ifrs.osorio.tads.aulabanco.repository;
 
 import br.edu.ifrs.osorio.tads.aulabanco.model.Endereco;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EnderecoRepository extends CrudRepository<Endereco, Integer> {
 }
